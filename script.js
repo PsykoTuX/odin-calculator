c7307104765ab6b34be14ce55bcce3d6cfa0b25c
Arthur <arthur.guilbert2708@gmail.com>
2022-07-22 10:03:23 -0400

    Create the function that does the actual math operations

--- script.js
+++ script.js
@@ -1,31 +1,56 @@
-// Functions: 
 
-const test=[1,2,3,4,5]
+let result = null;
+let operator = null;
 
-function add(array) { 
-    sum = array.reduce((total, item) => {
-       return total + item;
-    });
-    console.log(sum)
-};
+// Ask for the type of operation 
+operator = prompt("Choose operator: 'addition', 'subtraction', 'multiplication' or 'division'");
 
-function substract(array) {
-    difference = array.reduce((total, item) => {
-        return total - item;
-    });
-    console.log(substraction)
-};
+function operate(operator, aNumber, bNumber) {
+// Basic math operators functions: 
+    function add(array) { 
+        sum = array.reduce((total, item) => {
+           return total + item;
+        });
+        console.log(sum)
+    };
+    
+    function substract(array) {
+        difference = array.reduce((total, item) => {
+            return total - item;
+        });
+        console.log(difference);
+    };
+    
+    function multiply(array) {
+        product = array.reduce((total, item) => {
+            return total * item;
+        });
+        console.log(product);
+    };
+    
+    function divide(array) {
+        quotient = array.reduce((total, item) => {
+            return total / item;
+        });
+        console.log(quotient);
+    };
 
-function multiply(array) {
-    product = array.reduce((total, item) => {
-        return total * item;
-    });
-    console.log(product)
-};
 
-function divide(array) {
-    quotient = array.reduce((total, item) => {
-        return total / item;
-    });
-    console.log(quotient)
-};
+//Actual operation 
+let array = [aNumber, bNumber];
+if (operator=="addition") {
+    result=add(array);
+    console.log(result);
+    } else if (operator=="subtraction") {
+        result=substract(array);
+    } else if (operator=="multiplication") {
+        result==multiply(array);
+    } else if (operator=="division") {
+        result==divide(array);    
+    } else {
+        console.log("Please enter a valid operation: 'addition', 'subtraction',\
+            'multiplication' or 'division'");
+        operate();
+    }
+    
+}
