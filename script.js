3af95067c5c5f3311f62ff0392f3015cf19553fd
Arthur <arthur.guilbert2708@gmail.com>
2022-07-23 11:19:35 -0400

    Make the calculator work for basic operations

--- script.js
+++ script.js
@@ -2,24 +2,34 @@
 let result = '';
 let operator = '';
 let displayValue = '';
-
+let storedValue = '';
 // Ask for the type of operation 
 //operator = prompt("Choose operator: 'addition', 'subtraction', 'multiplication' or 'division'");
 
 function operate(operator, aNumber, bNumber) {
+    aNumber= parseFloat(aNumber);
+    bNumber=parseFloat(bNumber);
+    
+    let array = [aNumber, bNumber];
+    console.log(array);
+        //Actual operations 
+
+        
     // Basic math operators functions: 
     function add(array) { 
         sum = array.reduce((total, item) => {
            return total + item;
         });
-        console.log(sum)
+        console.log(sum);
+        return sum;
     };
 
-    function substract(array) {
+    function subtract(array) {
         difference = array.reduce((total, item) => {
             return total - item;
         });
         console.log(difference);
+        return difference;
     };
     
     function multiply(array) {
@@ -27,6 +37,7 @@ function operate(operator, aNumber, bNumber) {
             return total * item;
         });
         console.log(product);
+        return product;
     };
     
     function divide(array) {
@@ -34,32 +45,34 @@ function operate(operator, aNumber, bNumber) {
             return total / item;
         });
         console.log(quotient);
+        return quotient;
     };
 
-    //Actual operation 
-    let array = [aNumber, bNumber];
     if (operator=="addition") {
         result=add(array);
+        return sum;
         } else if (operator=="subtraction") {
-            result=substract(array);
+            result=subtract(array);
+            return difference;
         } else if (operator=="multiplication") {
             result==multiply(array);
+            return product;
         } else if (operator=="division") {
-            result==divide(array);    
-        } else {
-            console.log("Please enter a valid operation: 'addition', 'subtraction',\
-                'multiplication' or 'division'");
-            operate();
-        }
+            result==divide(array);
+            return quotient;
+        } ;
 }
 
 // DOM manipulation
 
-const buttons = document.querySelectorAll('button');
-const screen = document.querySelector('#screen')
-const content = document.createElement('p')
+const buttons = document.querySelectorAll('.digit');
+const operators = document.querySelectorAll('.operator');
+const screen = document.querySelector('#screen');
+const content = document.createElement('p');
 screen.appendChild(content);
 const eraseButton = document.getElementById('clear-button');
+const equalButton = document.getElementById('equal');
+
 
 function addContent(input) {
     content.textContent=+ input;
@@ -76,18 +89,56 @@ buttons.forEach((button) => {
     });
 });
 
+operators.forEach((item) => {
+    item.addEventListener('click', () => {
+        operator=item.id;
+        console.log(storedValue);
+
+        if (storedValue == '') {
+            storedValue = displayValue;
+        }
+
+
+        
+        displayValue = ''
+    })
+    });
+    
+
+
+function calculusResult() {
+    console.log(operator);
+    console.log(storedValue);
+    console.log(displayValue);
+    
+    storedValue = parseFloat(storedValue);
+    displayValue = parseFloat (displayValue);
+    
+    result=operate(operator, storedValue, displayValue);
+    addContent(result);
+    storedValue = result;
+    console.log(result);
+    console.log(storedValue);
+    result = ''
+
+}
+
 //Result button 
 
-
+equalButton.addEventListener('click', () => {
+    calculusResult()
+});
 
 
 //Clear button 
 function reset() {
 content.textContent='';
+displayValue='';
 }
 
 eraseButton.addEventListener('click', () => {
     displayValue = '';
+    storedValue = '';
     reset();
     
     }
